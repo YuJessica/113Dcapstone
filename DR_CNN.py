02f21b6b9bd7b3cefeeb9aeceea53c975efd790e
ranavarro2 <161097016+ranavarro2@users.noreply.github.com>
2024-03-05 14:45:17 -0800

    Update DR_CNN.py

--- DR_CNN.py
+++ DR_CNN.py
@@ -78,15 +78,38 @@ model.load_weights(weights) #Load weights from the pre-trained model (Colab)
 ### CNN Model END ###
 
 ### Prediction START ###
-result = model.predict(immatrix)
+import gpiod
+import time
+
+LED = 0
 if(result == 0)
     # TODO: Code to light up LED 0
+    LED = 14
 elif(result == 1)
     # TODO: Code to light up LED 1
+    LED = 15
 elif(result == 2)
     # TODO: Code to light up LED 2
+    LED = 18
 elif(result == 3)
     # TODO: Code to light up LED 3
+    LED = 23
 elif(result == 4)
     # TODO: Code to light up LED 4
+    LED = 24
+
+chip = gpiod.Chip('gpiochip4')
+led_line = chip.get_line(LED_PIN)
+led_line.request(consumer="LED",type=gpiod.LINE_REQ_DIR_OUT)
+result = model.predict(immatrix)
+
+i = 0
+try:
+ while i<10:
+    led_line.set_value(1)
+    time.sleep(1)
+    i+=1
+finally:
+ led_line.release()
+i = 0
 ### Prediction END ###
