1e7c0d83235105edfabb61e49bf6489eb5e36149
ranavarro2 <161097016+ranavarro2@users.noreply.github.com>
2024-03-10 22:51:42 -0700

    Update DR_CNN.py

--- DR_CNN.py
+++ DR_CNN.py
@@ -78,9 +78,9 @@ import gpiod
 import time
 
 prediction = model.predict(imgMatrix) # outputs an array of size equal to the number of classes (5), predicted result is the ith index
-result = prediction[0]
+result = 0
 for i in prediction:
-    if prediction[i] > result: 
+    if i > result: 
         result = i
 # Turn on Corresponding LED to display the result
 LED = 0
