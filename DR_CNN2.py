c75bd757dcf3d041e5c85d603e59efcc8e55d1a8
ranavarro2 <161097016+ranavarro2@users.noreply.github.com>
2024-03-18 22:20:56 -0700

    Update DR_CNN2.py

--- DR_CNN2.py
+++ DR_CNN2.py
@@ -59,6 +59,7 @@ def loadImage(imgName, folderPath):
     # name is basename of image, e.g., 16_right.jpeg
     # folderPath is path to directory of images
     image = load_ben_color(folderPath + imgName) 
+    print(folderPath + imgName)
     image = np.expand_dims(image, axis=0) # Convert image to tensor
     image = normalize(image)
     return image
@@ -138,7 +139,6 @@ for classNum in range(NUM_CLASSES):
     print(f"Testing images of type {classNum}.")
     folderPath = f'/mnt/usb1/images/{classNum}/'
     for imgFile in os.listdir(folderPath):
-        print(folderPath)
         imgName = os.path.basename(imgFile)
         testImage = loadImage(imgName, folderPath)
         prediction = model.predict(testImage, verbose = 0) # outputs an array of size equal to the number of classes (5), predicted result is the ith index
